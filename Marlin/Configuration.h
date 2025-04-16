cd7d75d413462e8a77520b43c79f5e443fb8906c
nelo360 <nelo360@gmail.com>
2025-04-16 18:40:32 +0200

    Slow z homing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2374,7 +2374,7 @@
 #endif
 
 // Homing speeds (linear=mm/min, rotational=°/min)
-#define HOMING_FEEDRATE_MM_M { (100*60), (100*60), (25*60) }
+#define HOMING_FEEDRATE_MM_M { (100*60), (100*60), (15*60) }
 
 // Edit homing feedrates with M210 and MarlinUI menu items
 //#define EDITABLE_HOMING_FEEDRATE
